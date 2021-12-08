ae6bebd8a5ad6c614b8e9624cf6b4fe675f748d4
Dylan Vu <dylanvu92gmail.com>
2021-12-07 16:08:12 -0800

    include typings

---
+++ typings/express.d.ts
@@ -0,0 +1,3 @@
+// Express and cron don't have types so follow this: https://stackoverflow.com/questions/41292559/could-not-find-a-declaration-file-for-module-module-name-path-to-module-nam/42505940#42505940
+declare module 'express';
+declare module 'cron';
