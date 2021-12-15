1470fd68dd6112f454a8b8401f5ea9711a0ec5b7
Dylan Vu <dylanvu92gmail.com>
2021-12-15 02:25:29 -0800

    fix file extension

--- bin/createLog.ts
+++ bin/createLog.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as moment from 'moment';
 
 export const CreateErrorLog = (errorData: any) => {
-    fs.writeFile(`../error-logs/${moment().format()}`, errorData, (err: any) => {
+    fs.writeFile(`../error-logs/${moment().format()}.txt`, errorData, (err: any) => {
         if (err) {
             return console.error(err);
         }
--- dist/bin/createLog.js
+++ dist/bin/createLog.js
@@ -4,7 +4,7 @@ exports.CreateErrorLog = void 0;
 var fs = require("fs");
 var moment = require("moment");
 var CreateErrorLog = function (errorData) {
-    fs.writeFile("../error-logs/".concat(moment().format()), errorData, function (err) {
+    fs.writeFile("../error-logs/".concat(moment().format(), ".txt"), errorData, function (err) {
         if (err) {
             return console.error(err);
         }
